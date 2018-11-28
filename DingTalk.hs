b00080d6ddc80f27a684258069e680fd53fc12e1
Gilbert Fine <gilbert.fine@gmail.com>
2018-11-28 08:36:28 +0800

    re-export Callback from top module

--- DingTalk.hs
+++ DingTalk.hs
@@ -9,6 +9,7 @@ module DingTalk
   , module DingTalk.OAPI.Message.Chat
   , module DingTalk.OAPI.Message.CorpConversation
   , module DingTalk.OAPI.Process
+  , module DingTalk.OAPI.Callback
   , module DingTalk.JS
   , module DingTalk.Yesod
   , module DingTalk.Misc
@@ -27,3 +28,4 @@ import DingTalk.Yesod
 import DingTalk.Misc
 import DingTalk.OAPI.Message.CorpConversation
 import DingTalk.OAPI.Process
+import DingTalk.OAPI.Callback
