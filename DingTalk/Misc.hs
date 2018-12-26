88d20abc5fe7b1801efb3bb4425a977f2518f5cd
Gilbert Fine <gilbert.fine@gmail.com>
2018-12-26 16:38:18 +0800

    two help functions: logDingTalkError_, logDingTalkError

--- DingTalk/Misc.hs
+++ DingTalk/Misc.hs
@@ -2,9 +2,12 @@ module DingTalk.Misc where
 
 -- {{{1 imports
 import ClassyPrelude
+import Control.Monad.Logger
 import qualified Data.Text            as T
+import Text.Show.Unicode (ushow)
 
 import DingTalk.Types
+import DingTalk.OAPI.Basic
 -- }}}1
 
 
@@ -28,6 +31,23 @@ rawTextToCorpIdOrSuiteKey t =
      then Left $ CorpId t
      else Right $ SuiteKey t
 
+
+logDingTalkError :: MonadLogger m => m a -> Either OapiError a -> m a
+-- {{{1
+logDingTalkError f err_or = do
+  case err_or of
+    Right x -> return x
+    Left err -> do
+        logDingTalkError_ err
+        f
+-- }}}1
+
+
+logDingTalkError_ :: MonadLogger m => OapiError -> m ()
+-- {{{1
+logDingTalkError_ err = do
+  $logError $ "DingTalk API error: " <> fromString (ushow err)
+-- }}}1
 
 
 -- vim: set foldmethod=marker:
